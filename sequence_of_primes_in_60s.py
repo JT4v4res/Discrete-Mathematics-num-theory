4e4c9ba13fb845d2adfb5b9f77ad49def653c7c5
João Tavares <jvst@ic.ufal.br>
2020-01-11 19:23:24 -0300

    Update and rename seq_primos_60s.py to sequence_of_primes_in_60s.py

--- sequence_of_primes_in_60s.py
+++ sequence_of_primes_in_60s.py
@@ -6,7 +6,7 @@ import time, sys
 #catching the limit time
 end_time = time.time() + 60	
 
-def primao():
+def is_prime():
 	x = 1
 	i = 2
 	c = 0
@@ -14,12 +14,16 @@ def primao():
 	while time.time() < end_time:
 		c = 0
 		for i in range(2,x):
-			#if the number has another divisor, c is incremented
-			if (x % i == 0):
-				c += 1
-		#if c isn't 0, the number was divided by more numbers than 1 and himself
+		  if x == 2:
+		    break
+		  elif x % 2 == 0:
+		    c += 1
+		    break
+		  elif x % i == 0:
+		    c += 1
+		    break
 		if (c == 0):
 			print(x)
 		x += 1
 
-primao()
+is_prime()
